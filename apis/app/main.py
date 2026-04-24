15ae7172640c3ea68e6df515b74aba6aee8b1694
KyleC55 <kc5371@nyu.edu>
2026-04-23 22:29:37 -0400

    Add PORT env var support and CI/CD setup

--- apis/app/main.py
+++ apis/app/main.py
@@ -36,4 +36,5 @@ def get_classes():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ["API_INTERNAL_PORT"]))
+    port = int(os.environ.get("PORT") or os.environ.get("API_INTERNAL_PORT", "8000"))
+    app.run(host="0.0.0.0", port=port)
