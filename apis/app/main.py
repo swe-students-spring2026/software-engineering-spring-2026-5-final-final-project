4af76c0a4957ade866e4e0d59815a94631c71714
avf8449 <avf8449@nyu.edu>
2026-04-23 21:06:50 -0400

    added mongodb atlas to docker and a very simple test for course retrieval

--- apis/app/main.py
+++ apis/app/main.py
@@ -1,14 +1,25 @@
 import os
 
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient(os.environ["MONGO_URI"])
+db = client[os.environ["MONGO_DB_NAME"]]
 
 
 @app.get("/health")
 def health():
     return jsonify({"status": "ok", "service": "apis"})
+
+
+@app.get("/classes")
+def get_classes():
+    term = request.args.get("term")
+    query = {"term_code": term} if term else {}
+    classes = list(db.classes.find(query, {"_id": 0}))
+    return jsonify(classes)
 
 
 if __name__ == "__main__":
