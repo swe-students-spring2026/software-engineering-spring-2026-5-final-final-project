329078bbe9b92bc2f66b219c4a3d7ece06d5c6cf
hanlinyan-dev <hy2484@nyu.edu>
2026-04-26 16:38:14 -0400

    use spotify oauth to login

--- web-app/app.py
+++ web-app/app.py
@@ -7,16 +7,13 @@ import pymongo
 from bson.objectid import ObjectId
 from dotenv import load_dotenv
 from flask import Flask, flash, redirect, render_template, request, url_for
-from flask_login import LoginManager, UserMixin, current_user, login_required, login_user, logout_user
-from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import timedelta
 from flask import Flask, render_template, request
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 # create the Flask app
 app = Flask(__name__)
-# user login manager
-login_manager = LoginManager()
-login_manager.init_app(app)
 # load environment variables from .env file
 load_dotenv()
 # set the secret key for session management
@@ -25,17 +22,10 @@ app.secret_key = os.getenv("SECRET_KEY")
 client = pymongo.MongoClient(os.getenv("MONGO_URI"))
 db = client[os.getenv("MONGO_DB_NAME")]
 
-# user model for Flask-Login
-class User(UserMixin):
-    def __init__(self, user_data):
-        self.id = str(user_data["_id"])
-        self.username = user_data["username"]
-        self.password_hash = user_data["password_hash"]
-
-# user loader callback for Flask-Login
-@login_manager.user_loader
-def load_user(user_id):
-    user_data = db.users.find_one({"_id": ObjectId(user_id)})
-    if user_data:
-        return User(user_data)
-    return None
+# set up Spotify API credentials
+sp_oauth = SpotifyOAuth(
+    client_id=os.getenv("SPOTIFY_CLIENT_ID"),
+    client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
+    redirect_uri=os.getenv("SPOTIFY_REDIRECT_URI"),
+    scope="playlist-modify-public playlist-modify-private",
+)
