046d209c4d19d0674767a5997f69d2b3b717d195
diyagreben <dhg9058@nyu.edu>
2026-04-28 17:18:16 -0400

    added basic webapp scaffolding

---
+++ web_app/app.py
@@ -0,0 +1,22 @@
+import os
+import pymongo
+from flask import Flask, render_template
+from dotenv import load_dotenv
+
+load_dotenv()
+MONGO_URI = os.getenv('MONGO_URI')
+MONGO_DBNAME = os.getenv('DB_NAME')
+
+app = Flask(__name__)
+
+connection = pymongo.MongoClient(MONGO_URI)
+db = connection[MONGO_DBNAME]
+
+
+@app.route('/')
+def show_tasks():
+    all_tasks = db.tasks.find({})
+    return render_template('index.html', tasks=all_tasks)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5004)
