7e7ffb3e5f019f651cdb42cd86a012d8e40f91b0
yashpaz123 <yrp6541@nyu.edu>
2026-04-29 12:42:06 -0400

    setup basic flask app and pipenv install

---
+++ machine-learning-client/src/main.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.post('/api/get_priority_score')
+def get_priority_score():
+    data = request.get_json()
+    # TODO: impl
+    score = 0
+    return jsonify({"score": score})
+
+app.run()
