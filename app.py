ba7aa6de24eaa0f4c3712282f6672772d7b42182
grace350 <gaj8834@nyu.edu>
2026-04-29 16:45:03 -0400

    separate by date

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
 from flask import Flask, render_template, redirect, request
+import json
 
 app = Flask(__name__)
 
+overdue = []
+due_soon = []
+upcoming = []
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', overdue=overdue, due_soon=due_soon, upcoming=upcoming)
 
 @app.route('/submit_new_task', methods=['POST'])
 def submit_new_task():
     form_data = request.json
-    return redirect('/')
+    if form_data.get('status') == 'overdue':
+        overdue.append(form_data)
+    elif form_data.get('status') == 'due_soon':
+        due_soon.append(form_data)
+    elif form_data.get('status') == 'upcoming':
+        upcoming.append(form_data)
+
+    payload = {
+        'status': 'success'
+    }
+    return json.dumps(payload)
 
 
 if __name__ == '__main__':
