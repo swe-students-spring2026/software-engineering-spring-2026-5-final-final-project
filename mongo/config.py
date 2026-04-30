5addb14ff898e4c97cf22de847d7881c2fe1fb4f
Hudhaifah Rehman <hr2409@nyu.edu>
2026-04-30 16:07:11 -0400

    fixed mongo config

--- mongo/config.py
+++ mongo/config.py
@@ -3,10 +3,13 @@ import os
 import pymongo
 
 class Config:
+    load_dotenv()
     #Load variables from .env file
     MONGO_DBNAME = os.getenv('MONGO_DBNAME')
     MONGO_URI = os.getenv('MONGO_URI')
 
     #Connect to Mongo
-    connection = pymongo.MongoClient(MONGO_URI)
-    db = connection[MONGO_DBNAME]
+    def connect_to_db():
+        connection = pymongo.MongoClient(MONGO_URI)
+        db = connection[MONGO_DBNAME]
+        return db
