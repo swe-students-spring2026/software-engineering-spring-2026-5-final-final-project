974e1a67d654db7e1cc9dfbcd09677fc3122168a
Hudhaifah Rehman <hr2409@nyu.edu>
2026-04-30 15:36:05 -0400

    mongo setup complete?

--- mongo/mongo_setup.py
+++ mongo/mongo_setup.py
@@ -2,9 +2,11 @@ from dotenv import load_dotenv
 import os
 import pymongo
 
-#Load variables from .env file
-MONGO_DBNAME = os.getenv('MONGO_DBNAME')
-MONGO_URI = os.getenv('MONGO_URI')
+class Config:
+    #Load variables from .env file
+    MONGO_DBNAME = os.getenv('MONGO_DBNAME')
+    MONGO_URI = os.getenv('MONGO_URI')
 
-#Connect to Mongo
-connection = pymongo.MongoClient(MONGO_URI)
+    #Connect to Mongo
+    connection = pymongo.MongoClient(MONGO_URI)
+    db = connection[MONGO_DBNAME]
