e78f6cc18cfacbd495016ce5422346ffb1395544
OverYander <gavinchen8706@gmail.com>
2026-04-29 20:06:40 -0400

    folder structure

--- webapp/app.py
+++ webapp/app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
+
+
+# python -m pipenv run python app.py
