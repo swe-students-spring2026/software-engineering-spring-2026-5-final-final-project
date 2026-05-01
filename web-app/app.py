b5fe830554094a09aef8151d4a7dde3e48588aab
YechengYueEddy <yy4908@nyu.edu>
2026-05-01 14:58:12 -0400

    fix auth by storing users in mongodb

--- web-app/app.py
+++ web-app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, request
 import logging
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 import json
 import os
 import requests
@@ -28,37 +28,44 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login_page'
 
-users = {} #    REPLACE W MONGODB
-
 class User(UserMixin):
     pass
 
 @login_manager.user_loader
-def user_loader(username):
-    if username not in users:
-        return
+def user_loader(user_email):
+    user_doc = mongo.db.users.find_one({"user_email": user_email})
+    if not user_doc:
+        return None
     user = User()
-    user.id = username
+    user.id = user_email
     return user
 
 @login_manager.request_loader
 def request_loader(request):
-    username = request.form.get('username')
-    if username not in users:
-        return
+    user_email = request.form.get('username')
+    if not user_email:
+        return None
+    user_doc = mongo.db.users.find_one({"user_email": user_email})
+    if not user_doc:
+        return None
     user = User()
-    user.id = username
+    user.id = user_email
     return user
+
+def add_id(tasks):
+    for task in tasks:
+        task['id'] = str(task['_id'])
+    return tasks
 
 @app.route('/')
 @login_required
 def index():
-    user = "test_user"
+    user = current_user.id
 
-    overdue = mongo.get_assignments_by_status(user, "overdue")
-    due_soon = mongo.get_assignments_by_status(user, "due_soon")
-    upcoming = mongo.get_assignments_by_status(user, "upcoming")
-    completed = mongo.get_assignments_by_status(user, "completed")
+    overdue = add_id(mongo.get_assignments_by_status(user, "overdue"))
+    due_soon = add_id(mongo.get_assignments_by_status(user, "due_soon"))
+    upcoming = add_id(mongo.get_assignments_by_status(user, "upcoming"))
+    completed = add_id(mongo.get_assignments_by_status(user, "completed"))
 
     return render_template(
         'index.html',
@@ -69,6 +76,7 @@ def index():
     )
 
 @app.route('/submit_new_task', methods=['POST'])
+@login_required
 def submit_new_task():
     data = request.json
 
@@ -77,16 +85,16 @@ def submit_new_task():
             "title": data.get("title"),
             "course": data.get("course"),
             "description": data.get("description"),
-            "due_date": data.get("due_date")
+            "due_date": data.get("date")
         })
         ml_data = ml_response.json()
 
         mongo.add_assignment(
-            user_email="test_user",
+            user_email=current_user.id,
             title=data.get("title"),
             course=data.get("course"),
             description=data.get("description"),
-            due_date=datetime.strptime(data.get("due_date"), "%Y-%m-%d"),
+            due_date=datetime.strptime(data.get("date"), "%Y-%m-%d"),
             estimated_hours=ml_data.get("estimated_hours"),
             difficulty=ml_data.get("difficulty"),
             priority=ml_data.get("priority"),
@@ -111,7 +119,8 @@ def login():
     username = request.form.get('username')
     password = request.form.get('password')
 
-    if username not in users or users[username]['password'] != password:
+    user_doc = mongo.db.users.find_one({"user_email": username})
+    if not user_doc or user_doc['password'] != password:
         return """<div>wrong username or password</div>
                 <a href="/login"> go back to login </a>"""
 
@@ -125,12 +134,11 @@ def register():
     username = request.form.get('username')
     password = request.form.get('password')
 
-    if username in users:
+    if mongo.db.users.find_one({"user_email": username}):
         return """<div>username already exists</div>
                 <a href="/login"> go to login </a>"""
 
-    users[username] = {'password': password}
-    print(users)
+    mongo.db.users.insert_one({"user_email": username, "password": password})
     return redirect('/login')
 
 def find_task(task_id):
@@ -138,7 +146,7 @@ def find_task(task_id):
     return next((task for task in all_tasks if task['id'] == task_id), None)
 
 # detail page
-@app.route('/task/<int:task_id>')
+@app.route('/task/<task_id>')
 def task_detail(task_id):
     task = find_task(task_id)
     if task is None:
@@ -146,7 +154,7 @@ def task_detail(task_id):
 
     return render_template('detail.html', task=task)
 
-@app.route('/task/<int:task_id>/edit', methods=['GET', 'POST'])
+@app.route('/task/<task_id>/edit', methods=['GET', 'POST'])
 def edit_task(task_id):
     task = find_task(task_id)
     if task is None:
@@ -163,7 +171,7 @@ def edit_task(task_id):
     
     return render_template('edit.html', task=task)
 
-@app.route('/complete_task/<int:task_id>')
+@app.route('/complete_task/<task_id>')
 def complete_task(task_id):
     task = find_task(task_id)
     if task is None:
