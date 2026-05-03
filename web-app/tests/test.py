fadc19c487a22a40bf799dea15c8b1b205a9ae7f
rehanguptaNYU <rehang2005@gmail.com>
2026-05-02 22:30:36 -0400

    login route unit testing

--- web-app/tests/test.py
+++ web-app/tests/test.py
@@ -4,12 +4,20 @@ def test_register(client):
 
 def test_register_empty_fields(client):
     response=client.post("/register",data={
+        "_id":"", 
         "username":"",
         "email":"",
         "password":""
     })
+    assert response.status_code==302
     assert "/register" in response.location
 def test_register_username_exists(client,monkeypatch):
+     test_user={
+          "_id":"userId123",
+          "username":"rehan",
+          "email":"rehang2005@gmail.com",
+          "password":"Rehan123@2005"
+     }
      monkeypatch.setattr(
           "app.find_user_by_username",lambda username:{"username":username}
      )
@@ -21,7 +29,8 @@ def test_register_username_exists(client,monkeypatch):
      assert response.status_code==302
      assert "/register" in response.location
 def test_register_new_username(client,monkeypatch):
-     fakeUser={
+     test_user={
+          "_id":"userId123",
           "username":"rehan",
           "email":"rehang2005@gmail.com",
           "password":"Rehan123@2005"
@@ -35,8 +44,72 @@ def test_register_new_username(client,monkeypatch):
           lambda username, email, hashed_password:"userid123"
      )
      monkeypatch.setattr(
-          "app.find_user_by_id",lambda user_id: fakeUser
+          "app.find_user_by_id",lambda user_id: test_user
      )
-     response=client.post("/register",data=fakeUser)
+     response=client.post("/register",data={
+          "username":"rehan",
+          "email":"rehang2005@gmail.com",
+          "password":"Rehan123@2005"
+     })
      assert response.status_code==302
      assert "/dashboard" in response.location
+def test_login(client):
+     response=client.get("/login")
+     assert response.status_code == 200
+def test_login_username_not_found(client,monkeypatch):
+     test_user={
+          "_id":"userId123",
+          "username":"rehan",
+          "email":"rehang2005@gmail.com",
+          "password":"Rehan123@2005"
+     }
+     monkeypatch.setattr(
+          "app.find_user_by_username",lambda username:None
+     )
+     response=client.post("/login",data={
+          "username":"rehan",
+          "email":"rehang2005@gmail.com",
+          "password":"Rehan123@2005"
+     })
+     assert response.status_code==302
+     assert "/login" in response.location
+def test_login_wrong_password(client,monkeypatch):
+     test_user={
+          "_id":"userId123",
+          "username":"rehan",
+          "email":"rehang2005@gmail.com",
+          "password":"Rehan123@2005"
+     }
+     monkeypatch.setattr(
+          "app.find_user_by_username",lambda username:test_user
+     )
+     monkeypatch.setattr(
+          "app.bcrypt.check_password_hash",lambda hashed_password, password:False 
+     )
+     response=client.post("/login",data={
+          "username":"rehan",
+          "password":"pass123"
+     })
+     assert response.status_code == 302
+     assert "/login" in response.location
+def test_login_successful(client,monkeypatch):
+     test_user={
+          "_id":"userId123",
+          "username":"rehan",
+          "email":"rehang2005@gmail.com",
+          "password":"Rehan123@2005"
+     }
+     monkeypatch.setattr(
+          "app.find_user_by_username",lambda username:test_user
+     )
+     monkeypatch.setattr(
+          "app.bcrypt.check_password_hash",lambda hashed_password, password:True
+     )
+     response=client.post("/login",data={
+          "username":"rehan",
+          "password":"Rehan123@2005"
+     })
+     assert response.status_code == 302
+     assert "/dashboard" in response.location
+
+     
