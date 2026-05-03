e28c2d5ae4917d1a1fe3002ddf8d11ebd6ae2633
rehanguptaNYU <rehang2005@gmail.com>
2026-05-02 22:55:33 -0400

    test commit

--- web-app/tests/test.py
+++ web-app/tests/test.py
@@ -1,8 +1,4 @@
-def test_user_opens_app_without_login(client):
-     response=client.get("/")
-     assert response.status_code==302
-     assert "/login" in response.location
-     
+
 def test_register(client):
      response=client.get("/register")
      assert response.status_code==200
