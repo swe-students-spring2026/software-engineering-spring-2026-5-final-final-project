e7c41e2abc098f4329fb179152a1f340660c63c5
EdwarddYe <edwardye13@gmail.com>
2026-05-04 03:29:22 -0400

    implemented email sender and 60 sec checker pt2

--- reminder_worker/worker.py
+++ reminder_worker/worker.py
@@ -1,11 +1,12 @@
 import time
+import traceback
 from reminder_service import process_due_reminders
 from db import get_tasks_collection
 from email_service import send_task_reminder
 
 def run_worker():
     tasks_collection = get_tasks_collection()
-    print("Worker started ")
+    print("Reminder worker started")
 
     while True:
         try:
@@ -13,10 +14,15 @@ def run_worker():
                 tasks_collection,
                 send_task_reminder
             )
-            print(f"Processed {count} reminders ")
+
+            print(f"[OK] Processed {count} reminders")
+
         except Exception as e:
-            print("Worker error: ", e)
-        time.sleep(60) 
-#waiting 60 seconds
+            print("[WORKER ERROR]")
+            print(e)
+            traceback.print_exc()
+        time.sleep(60)
+#wait 60 secs
+
 if __name__ == "__main__":
     run_worker()
